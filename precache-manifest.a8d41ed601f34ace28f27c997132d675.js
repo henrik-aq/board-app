18203664a9573729f03d69d7e4d5856ace9ca1b4
henrik-aq <henrik.qvarfordt@gmail.com>
2019-01-18 16:13:57 +0100

    Updates

---
+++ precache-manifest.a8d41ed601f34ace28f27c997132d675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d0cb50aaa8b0695dcab",
+    "url": "/board-app/static/js/runtime~main.0d0cb50a.js"
+  },
+  {
+    "revision": "6efc9a290b0fa9b3e03d",
+    "url": "/board-app/static/js/main.6efc9a29.chunk.js"
+  },
+  {
+    "revision": "9e0ec6647f6f49ee599b",
+    "url": "/board-app/static/js/1.9e0ec664.chunk.js"
+  },
+  {
+    "revision": "6efc9a290b0fa9b3e03d",
+    "url": "/board-app/static/css/main.0eeca3a5.chunk.css"
+  },
+  {
+    "revision": "9e0ec6647f6f49ee599b",
+    "url": "/board-app/static/css/1.c145278b.chunk.css"
+  },
+  {
+    "revision": "fd87647aa0a829784f69986bca88fe27",
+    "url": "/board-app/index.html"
+  }
+];
